d8162f614806c18eb76e5c1eab181a3e93969aa3
Mitch Ludkey <mludk92@hotmail.com>
2023-01-22 18:24:18 -0600

    answered the first stretch question

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -96,18 +96,26 @@ console.log(find(5,arrayTestFill)) //false
 // console.log(find(7,arrayTestFill)) //false
 // console.log(find(2,arrayTestFill)) //true
 
-/*
+
 // ----------------------
 // Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if(string[0] == letter || (string[0]==(letter.toLowerCase()))){
+  return true
 }
+  else {
+    return false 
+  }
+}
+//wanted to make sure that i could get this to work and ignore user error, by converting to lower case if they enter upper. 
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
+console.log( 'isFirstLetter - should say true', isFirstLetter('A', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
-
+console.log( 'isFirstLetter - should say false', isFirstLetter('Z', 'apple') );
+/*
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
   let sum = 0
